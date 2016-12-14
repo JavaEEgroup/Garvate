2a819e6959d765a47f115fa33c57e8b34da67073
DancingLinks <348368689@qq.com>
2016-12-14 14:06:29 +0800

    Merge =  -=

---
+++ src/main/java/com/gc/repository/repository/ProjcetRepository.java
@@ -0,0 +1,9 @@
+package com.gc.repository.repository;
+
+import com.gc.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProjcetRepository extends JpaRepository<Project, Long> {
+
+}
